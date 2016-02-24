40bd4dab606783a26d45801d79448e087d878c49
qetennyson <quincytennyson@yahoo.com>
2016-02-23 21:52:11 -0500

    added blog post, updated some CSS

---
+++ app/scripts/controllers/home.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name nodeblogApp.controller:HomeCtrl
+ * @description
+ * # HomeCtrl
+ * Controller of the nodeblogApp
+ */
+angular.module('nodeblogApp')
+  .controller('HomeCtrl', function () {
+    
+  });
